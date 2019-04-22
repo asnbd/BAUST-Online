0df6cca08d6683545d170e29c7774581db25dc01
Siam Abdullah <siam.baust@gmail.com>
2019-04-22 18:25:32 +0600

    teacher add complete

--- dashboard/teachers_add.php
+++ dashboard/teachers_add.php
@@ -258,70 +258,70 @@ if ($login_role == 0 || $login_role == 1){  //Owner or Admin
                         <div class="col-75">
                             <select name="teacherDistrict" id="teacherDistrict">
                                 <option value="" selected>Choose...</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
-                                <option value="Barguna">Barguna</option>
+                                <option value="Bagerhat">Bagerhat</option>
+                                <option value="Bandarban">Bandarban</option>
+                                <option value="Barguna">Barguna</option>
+                                <option value="Barisal">Barisal</option>
+                                <option value="Bhola">Bhola</option>
+                                <option value="Bogra">Bogra</option>
+                                <option value="Brahmanbaria">Brahmanbaria</option>
+                                <option value="Chandpur">Chandpur</option>
+                                <option value="Chittagong">Chittagong</option>
+                                <option value="Chuadanga">Chuadanga</option>
+                                <option value="Comilla">Comilla</option>
+                                <option value="Cox's Bazar">Cox's Bazar</option>
+                                <option value="Dhaka">Dhaka</option>
+                                <option value="Dinajpur">Dinajpur</option>
+                                <option value="Faridpur">Faridpur</option>
+                                <option value="Feni">Feni</option>
+                                <option value="Gaibandha">Gaibandha</option>
+                                <option value="Gazipur">Gazipur</option>
+                                <option value="Gopalganj">Gopalganj</option>
+                                <option value="Habiganj">Habiganj</option>
+                                <option value="Jaipurhat">Jaipurhat</option>
+                                <option value="Jamalpur">Jamalpur</option>
+                                <option value="Jessore">Jessore</option>
+                                <option value="Jhalakati">Jhalakati</option>
+                                <option value="Jhenaidah">Jhenaidah</option>
+                                <option value="Khagrachari">Khagrachari</option>
+                                <option value="Khulna">Khulna</option>
+                                <option value="Kishoreganj">Kishoreganj</option>
+                                <option value="Kurigram">Kurigram</option>
+                                <option value="Kushtia">Kushtia</option>
+                                <option value="Lakshmipur">Lakshmipur</option>
+                                <option value="Lalmonirhat">Lalmonirhat</option>
+                                <option value="Madaripur">Madaripur</option>
+                                <option value="Magura">Magura</option>
+                                <option value="Manikganj">Manikganj</option>
+                                <option value="Meherpur">Meherpur</option>
+                                <option value="Moulvibazar">Moulvibazar</option>
+                                <option value="Munshiganj">Munshiganj</option>
+                                <option value="Mymensingh">Mymensingh</option>
+                                <option value="Narail">Narail</option>
+                                <option value="Naogaon">Naogaon</option>
+                                <option value="Narayanganj">Narayanganj</option>
+                                <option value="Narsingdi">Narsingdi</option>
+                                <option value="Natore">Natore</option>
+                                <option value="Nawabganj">Nawabganj</option>
+                                <option value="Netrakona">Netrakona</option>
+                                <option value="Nilphamari">Nilphamari</option>
+                                <option value="Noakhali">Noakhali</option>
+                                <option value="Pabna">Pabna</option>
+                                <option value="Panchagarh">Panchagarh</option>
+                                <option value="Parbattya Chattagram">Parbattya Chattagram</option>
+                                <option value="Patuakhali">Patuakhali</option>
+                                <option value="Pirojpur">Pirojpur</option>
+                                <option value="Rangpur">Rangpur</option>
+                                <option value="Rajshahi">Rajshahi</option>
+                                <option value="Rajbari">Rajbari</option>
+                                <option value="Satkhira">Satkhira</option>
+                                <option value="Shariatpur">Shariatpur</option>
+                                <option value="Sherpur">Sherpur</option>
+                                <option value="Sirajganj">Sirajganj</option>
+                                <option value="Sunamganj">Sunamganj</option>
+                                <option value="Sylhet">Sylhet</option>
+                                <option value="Tangail">Tangail</option>
+                                <option value="Thakurgaon">Thakurgaon</option>
 
                             </select>
                             <div class="invalid-feedback">
@@ -341,52 +341,7 @@ if ($login_role == 0 || $login_role == 1){  //Owner or Admin
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="departmentDesc">Department Description</label>
-                        </div>
-                        <div class="col-75">
-                            <textarea name="deptDesc" id="departmentDesc" placeholder="Enter Department Description" rows="3"><?php echo $dept_row['description'];?></textarea>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="departmentHead">Department Head</label>
-                        </div>
-                        <div class="col-75">
-                            <select name="deptHead" id="departmentHead">
-                                <option value="">Choose...</option>
-                                <?php
-                                    if($dept_row['username'] == ""){
-                                        echo '<option value="-1" selected>Not Assigned</option>';
-                                    } else {
-                                        echo '<option value="-1">Not Assigned</option>';
-                                        echo '<option value="'. $dept_row['username'] . '" selected>'.$dept_row['head'] . '</option>';
-                                    }
-                                ?>
 
-                                <?php
-                                $sql = "SELECT username, name FROM teacher WHERE username NOT IN (SELECT head FROM department)";
-                                //                            $sql = "SELECT username, name FROM teacher";
-
-                                if($result = mysqli_query($db_conn, $sql)){
-                                    if(mysqli_num_rows($result) > 0){
-                                        while($row = mysqli_fetch_assoc($result)){
-                                            echo "<option value='" . $row['username'] . "'>" . $row['name'] . "</option>";
-                                        }
-                                    } else {
-
-                                    }
-                                } else {
-                                    die("Error: " . mysqli_connect_error($db_conn). " SQL: " . $sql);
-                                }
-                                ?>
-                            </select>
-                            <div id="invalid-dept-head" class="invalid-feedback">
-                                * Please select a department head.
-                            </div>
-                        </div>
-                    </div>
                     <input name="deptID" type="hidden" value="<?php echo $dept_row['id'] ?>">
                     <div class="row">
                         <button type='submit' class='btn btn-primary' style="float: right; margin-top: 15px">Save</button><br>
